fc334a20494a623d45c12e0ea2e00b800724ff64
Amit Bahree <github@desigeek.com>
2024-11-30 16:34:49 -0800

    Remove unused Dockerfile and HTML template; update index.html for improved content and server info display

--- serverinfo.sh
+++ serverinfo.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+# Log access details
+echo "Accessed serverinfo.sh at $(date) from IP: $(echo $REMOTE_ADDR || echo 'N/A')" >&2
+
+# Output CGI headers
 echo "Content-type: application/json"
 echo ""
+
+# Generate server information in JSON format
 echo "{"
 echo "\"hostname\": \"$(hostname)\","
 echo "\"ip_address\": \"$(hostname -i)\","
